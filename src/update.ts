bf2777eed3e18c88bd27655cd4e7038600274bac
andrey31453 <andrey31453@google.com>
2024-03-20 12:56:48 +0300

    f

--- src/update.ts
+++ src/update.ts
@@ -8,13 +8,9 @@ import { generates } from '~generates'
 
 const update = () => {
   const npm = new NPM(new Manifests(new M_Env()))
-
-  console.log('npm.updates: ', npm.updates)
-
   if (!npm.updates.length) return
 
   exec(new NPM_Install_To_Docker_CMD(npm.updates).value)
-
   new Generate_M_File(npm.manifests, new Generate(_bd_name.update)).generate(
     generates.update
   )
